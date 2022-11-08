b328828f2ec1cce075d193c632af342ff4fbf48e
Natalyfuture <shevchuknataly68@gmail.com>
2022-11-08 10:38:44 +0200

    add script.js

---
+++ homework2/js/script.js
@@ -0,0 +1,16 @@
+let userAge;
+let userFirstName;
+let userSecondName;
+
+
+let newUser;
+let shoppingList;
+let userOnline;
+let employeeSalary;
+let cursorCoordinates;
+
+
+let greetings = 'Hello, World!';
+userAge = 50;
+console.log(greetings);
+console.log('user Age:', userAge);
