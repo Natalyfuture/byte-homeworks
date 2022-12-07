b5090f4a0979fe56e6007ccec24888149796c666
Natalyfuture <shevchuknataly68@gmail.com>
2022-12-07 17:06:34 +0300

    add script.js

--- homework9/script/script.js
+++ homework9/script/script.js
@@ -1,7 +1,7 @@
 //       TASK1
-/* //        1.1
+//        1.1
 
-const sourceArray = [1, 2, -10, -2, 7];
+/* const sourceArray = [1, 2, -10, -2, 7];
 const booleanArray = sourceArray.map((item) => {
     if(item > 0){
         item = true;
@@ -10,6 +10,10 @@ const booleanArray = sourceArray.map((item) => {
     }
     return item;
 });
+
+
+
+const booleanArray  = sourceArray.map((item) => item > 0);
 console.log(booleanArray);
  */
 
@@ -80,3 +84,130 @@ console.log(average); */
 
 // TASK2
 
+
+
+/* //      TASK 2.1
+
+const logger = (element, index, array) => {
+    
+    console.log(`In array [${array}] on position ${index}: ${element}`);
+
+};
+logger(1, 0, [1, 2, 3]);
+
+function ownForEach (array, callback){
+  for(let i = 0; i < array.length; i++){
+    let element = array[i];
+    console.log(element);
+    let index = array.indexOf(element);
+    console.log(index);
+    callback(element, index, array);
+  }
+}
+
+ownForEach([1, 2, 3], logger); 
+ */
+
+/* //    TASK    2.2
+const increment = (number) => number + 1;
+
+function ownMap(arr, callback) {
+  for(let i = 0; i < arr.length; i++){
+    arr[i] = callback(arr[i]);
+  }
+  console.log(arr)
+}
+
+ownMap([1, 2, 3], increment);
+
+
+ */
+
+
+/* //TASK     2.3
+
+
+const isNegative = (number) => number < 0;
+
+function ownFilter (arr, callback) {
+  const negativeArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(callback(arr[i])){
+    negativeArr.push(arr[i]);
+  }
+  
+  }
+  console.log(negativeArr);
+}
+
+
+ownFilter([-2, 4, -1], isNegative); 
+ */
+
+
+//    TASK 3
+
+
+const data = [
+  {
+    name: "John",
+    age: 24,
+    position: "senior",
+    isActive: false,
+  },
+  {
+    name: "Peter",
+    age: 33,
+    position: "middle",
+    isActive: false,
+  },
+  {
+    name: "Sam",
+    age: 29,
+    position: "junior",
+    isActive: true,
+  },
+  {
+    name: "Mary",
+    age: 24,
+    position: "middle",
+    isActive: false,
+  },
+  {
+    name: "Steve",
+    age: 23,
+    position: "middle",
+    isActive: true,
+  },
+  {
+    name: "Kate",
+    age: 31,
+    position: "middle",
+    isActive: false,
+  },
+  {
+    name: "Sally",
+    age: 19,
+    position: "junior",
+    isActive: false,
+  },
+  {
+    name: "Jack",
+    age: 19,
+    position: "middle",
+    isActive: true,
+  },
+];
+
+const filterData= (list, findObj) => {
+  const resFiltration = list.filter(item =>
+     Object.keys(findObj).every(key =>
+    item[key] === findObj[key]));
+
+  return resFiltration;
+}
+
+
+/* console.log(filterData(data, { age: 23 })); */
+/* console.log(filterData(data,  { age: 24 })); */
+console.log(filterData(data,  { age: 19, position: "junior" }));
