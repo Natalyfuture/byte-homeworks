f7aa4e64f89f97c5f129620c4afddd92b251a88f
Natalyfuture <shevchuknataly68@gmail.com>
2022-12-17 20:46:24 +0200

    change script.js

--- homework10_Praktyka/script/script.js
+++ homework10_Praktyka/script/script.js
@@ -2,11 +2,11 @@ const actionButton = document.querySelector("button");
 actionButton.addEventListener("click", library);
 
 const books = [
-    {id: 1, author: "Фиджеральд", name: "Великий Гетсби", isReading: false, amount: 1},
-    {id: 2, author: "Толстой", name: "Анна Каренина", isReading: false, amount: 2},
-    {id: 3, author: "Оруел", name: "1984", isReading: false, amount: 5},
-    {id: 4, author: "Сервантес", name: "Дон Кихот", isReading: false, amount: 3},
-    {id: 5, author: "Достоевский", name: "Преступление и наказание", isReading: false, amount: 1},
+    {id: 1, author: "Фиджеральд", name: "Великий Гетсби", amount: 1},
+    {id: 2, author: "Толстой", name: "Анна Каренина", amount: 2},
+    {id: 3, author: "Оруел", name: "1984", amount: 1},
+    {id: 4, author: "Сервантес", name: "Дон Кихот", amount: 3},
+    {id: 5, author: "Достоевский", name: "Преступление и наказание", amount: 1},
 ];
 
 function library() {
@@ -34,7 +34,7 @@ function library() {
 
 function takeBook() {
     const availableBooksName = books
-    .filter((book) => !book.isReading)
+    .filter((book) => book.amount > 0)
     .map((book) => `- ${book.name}`)
     .join('\n');
 
@@ -58,15 +58,15 @@ function takeBook() {
         return;
     }
 
-    if(desireBook.isReading){
+    if(desireBook.amount < 1){
         alert('This book is reading by someone else.');
 
         return;
     }else{
+        desireBook.amount--;
         alert(`OK. Id your book is : ${desireBook.id}`)
-    }
 
-    desireBook.isReading = true;
+    }
 }
 
 function returnBook() {
@@ -79,7 +79,8 @@ function returnBook() {
         return;
     }else{
         alert('Thanks for returning the book. Did you like it?');
-        return bookId.isReading = false;
+        bookId.amount++;
+        
     }
 }
 
@@ -87,17 +88,31 @@ function addBook() {
     let name = prompt("Enter a book's name");
     let author = prompt("Enter the author's name");
 
-    name = name.trim();
-    author = author.trim();
+    name = name.trim().toLowerCase();
+    author = author.trim().toLowerCase();
 
+    const verificationBooksName = books.find((book) => book.name.toLowerCase() === name);
+    const verificationBookAuthor = books.find((book) => book.author.toLowerCase() === author);
+
+    if(verificationBooksName && verificationBookAuthor){
+        verificationBookAuthor.amount++;
+        console.log(verificationBookAuthor.amount);
+        
+    }else{
+        name = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
+    author = author.slice(0, 1).toUpperCase() + author.slice(1).toLowerCase();
     const newBook = {
         name,
         author,
         isReading: false,
         id: generatedId(),
+        amount: 1,
     }
+     books.push(newBook);
 
-    books.push(newBook);
+     
+}
+   
     alert('The book is added successfully');
 
 }
