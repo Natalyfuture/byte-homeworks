7f40146252449cbaba5b27212a6de0d13aa519e3
Natalyfuture <shevchuknataly68@gmail.com>
2022-12-25 20:19:30 +0200

    clean script.js

--- homework12/script/script.js
+++ homework12/script/script.js
@@ -16,18 +16,17 @@ const createTooltip = (btn, text) => {
     tooltipElem = document.createElement('div');
     tooltipElem.className = 'tooltip';
     tooltipElem.innerHTML = `${text}`;
+
     document.body.append(tooltipElem);
   
 
     let coords = btn.getBoundingClientRect();
-    console.log(coords);
 
     let left = coords.left + 32;
     if (left < 0) left = 0; 
 
-    let top = coords.top - tooltipElem.offsetHeight - 25;
-
-    tooltipElem.style.position = 'absolute';
+    let top = coords.top - tooltipElem.offsetHeight - 10;
+  
     tooltipElem.style.left = left + 'px';
     tooltipElem.style.top = top + 'px';
 
@@ -37,7 +36,7 @@ const createTooltip = (btn, text) => {
 
   
 button.onmouseout = function(e) {
-  console.log("button.onmouseout");
+ 
   if (tooltipElem) {
     tooltipElem.remove();
     tooltipElem = null;
